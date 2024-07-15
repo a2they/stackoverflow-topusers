9509e9ce6010d069c9d4ca40cad57679c1f42e28
Andrey Leonov <aleonov@gmail.com>
2024-07-14 23:13:02 -0400

    new project

---
+++ StackOverflowTopUsers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StackOverflowTopUsers
+//
+//  Created by Andrey Leonov on 2024-07-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StackOverflowTopUsers/StackOverflowTopUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackOverflowTopUsersApp.swift
+//  StackOverflowTopUsers
+//
+//  Created by Andrey Leonov on 2024-07-14.
+//
+
+import SwiftUI
+
+@main
+struct StackOverflowTopUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
